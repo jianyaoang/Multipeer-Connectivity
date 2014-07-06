aa7d2fa6374f7a94457a3441db29d5df31aaddec
jianyaoang <jianyaoang1@gmail.com>
2014-07-06 15:50:34 -0500

    enhance UI by alertView addition

--- Multipeer-Connectivity/ViewController.m
+++ Multipeer-Connectivity/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "AppDelegate.h"
 
-@interface ViewController ()
+@interface ViewController () <UIAlertViewDelegate>
+@property (strong, nonatomic) AppDelegate *appDelegate;
+@property (nonatomic) int secretNumber;
+@property (nonatomic) BOOL hasCreatedGame;
+@property (nonatomic) BOOL isGameRunning;
 @property (strong, nonatomic) IBOutlet UITextField *guessTextField;
 @property (strong, nonatomic) IBOutlet UITextView *textViewHistory;
 @property (strong, nonatomic) IBOutlet UIButton *sendButton;
@@ -21,12 +26,61 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    self.appDelegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
+    [self toggleSubviewsState:NO];
 }
 
 - (IBAction)onStartGameButtonPressed:(id)sender
 {
+    if (!self.isGameRunning)
+    {
+        UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"Multipeer Connectivity" message:@"Please enter a number between 1 to 10" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Start Game", nil];
+        //UIAlertViewStylePlainTextInput adds text field to alertView
+        av.alertViewStyle = UIAlertViewStylePlainTextInput;
+        [[av textFieldAtIndex:0] setKeyboardType:UIKeyboardTypeNumberPad];
+        [av show];
+    }
     
+    //check if input number is 1-10
+    if (self.secretNumber >=1 && self.secretNumber <= 10)
+    {
+        //message players that a new game has started
+        //convert the text input into NSData and send it
+        NSString *messageToSend = @"New Game";
+        NSData *messageAsData = [messageToSend dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *error;
+        
+        //withMode: reliable / unreliable 
+        [self.appDelegate.mpcHandler.session sendData:messageAsData toPeers:self.appDelegate.mpcHandler.session.connectedPeers withMode:MCSessionSendDataReliable error:&error];
+        
+        if (error)
+        {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        else
+        {
+            self.hasCreatedGame = YES;
+            self.isGameRunning = YES;
+            
+            [self.textViewHistory setText:@""];
+        }
+    }
+    else
+    {
+        //if user enters number not 1-10
+        UIAlertView *av = [[UIAlertView alloc]initWithTitle:@"Multipeer Connectivity" message:@"Please enter a number between 1 to 10" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Okay", nil];
+        [av show];
+    }
+}
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    //check if player press on Start Game and the style of UIAlertView
+    if (alertView.alertViewStyle == UIAlertViewStylePlainTextInput && buttonIndex == 1)
+    {
+        UITextField *textField = [alertView textFieldAtIndex:0];
+        self.secretNumber = [textField.text intValue];
+    }
 }
 
 - (IBAction)onSendButtonPressed:(id)sender
@@ -37,6 +91,13 @@
 - (IBAction)onCancelButtonPressed:(id)sender
 {
     
+}
+
+-(void)toggleSubviewsState:(BOOL)shouldEnable
+{
+    self.cancelButton.enabled = shouldEnable;
+    self.guessTextField.enabled = shouldEnable;
+    self.sendButton.enabled = shouldEnable;
 }
 
 
